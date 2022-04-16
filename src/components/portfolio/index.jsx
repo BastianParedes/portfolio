713898c36ce96f8a8139d4c84bc5e06985b2ffa7
Bastián <bastian.p@outlook.com>
2022-04-16 01:22:43 -0400

    A punto de separar estilos en proyectos izquierda y derecha

--- src/components/portfolio/index.jsx
+++ src/components/portfolio/index.jsx
@@ -1,47 +1,38 @@
 
 import React from 'react';
-import { FaGlobe } from 'react-icons/fa'
-import { BsGithub } from 'react-icons/bs'
+import { VscSignOut, VscGithub } from 'react-icons/vsc'
 
 import Section  from '../section';
 import styles from './styles.module.css';
+import stylesRight from './stylesRight.module.css';
+import stylesLeft from './stylesLeft.module.css';
 
 import projects from './projects.json';
 
-function Project(props) {
-    // const imagePath = process.env.PUBLIC_URL + '/images/projects/' + props.image;
+function Project( { index, name, link, image, github, description, technologies }) {
+    // const imagePath = process.env.PUBLIC_URL + '/images/projects/' + image;
     const imagePath = process.env.PUBLIC_URL + '/images/background/background.jpg';
     return (
         <div className={styles['project-card']}>
-            <img className={styles['image']} src={imagePath} alt={props.image}/>
-            <h2 className={styles['project-name']}>{props.name}</h2>
+            <img className={styles['image']} src={imagePath} alt={image}/>
+            <h2 className={styles['project-name']}>{name}</h2>
             <div className={styles['project-description-container']}>
-                <p className={styles['project-description']}>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi possimus nemo sapiente aliquid ducimus voluptate culpa laboriosam qui, deserunt, mollitia adipisci, doloremque quia? Qui, velit voluptas excepturi maiores vel necessitatibus!
-                </p>
+                <p className={styles['project-description']}>{description}</p>
             </div>
+            <span className={styles['project-technologies']}>{technologies.join(' ')}</span>
+            {link !== '' ? <a className={styles['link-website']} href={link} target='_blank' rel='noreferrer noopener'><VscSignOut /></a> : ''}
+            <a className={styles['link-github']} href={github} target='_blank' rel='noreferrer noopener'><VscGithub /></a>
         </div>
     );
 }
 
-{/* <div className={styles['project-card']}>
-    <a href={props.href} target='_blank' rel='noreferrer noopener'><img className={styles['project-image']} src={process.env.PUBLIC_URL + '/images/projects/' + props.src} alt={props.name}/></a>
-    <span className={styles['project-name']}>{props.name}</span>
-    <div className={styles['links-project-container']}>
-        <a className={styles['link-btn-project']} href={props.href} target='_blank' rel='noreferrer noopener'><FaGlobe /></a>
-        <a className={styles['link-btn-project']} href={props.githubhref} target='_blank' rel='noreferrer noopener'><BsGithub /></a>
-    </div>
-</div> */}
+
 
 export default function Portfolio() {
-
-
-
     return (
-        <Section id='portfolio' title='My portfolio'>
+        <Section id='portfolio' title='Portfolio'>
             <div className={styles['projects-container']}>
-                {projects.projects.map((info, index) => <Project key={index} name={info.name} link={info.link} image={info.image} github={info.github} />)}
-                
+                {projects.projects.map((info, index) => <Project key={index} index={index} projectInfo={info}name={info.name} link={info.link} image={info.image} github={info.github} description={info.description} technologies={info.technologies} />)}
             </div>
         </Section>
     );
